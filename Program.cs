5b9a093c73d5ffdccf3731708c18cf910ccde6f8
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-06-22 20:36:55 +0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System.Collections.Concurrent;
+using System.IO;
+using encryption;
+using decryption;
+using displayInputOutput;
+using System.Globalization;
+
+namespace caesarcypher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleEnter.EnterInConsole();
+        }
+    }
+}
